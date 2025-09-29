be6e01ce40c3cdd676a26f7b409a979d114b045a
aashishgupta43 <guptaaashish594@gmail.com>
2025-09-29 10:14:47 +0530

    first commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,19 +1,19 @@
 // api.js
 import axios from 'axios';
 
-// ---------------------------
-// Axios instance
-// ---------------------------
+
 
 console.log(process.env.REACT_APP_API_URL,"foififhfhhf");
 
-const API = axios.create({
+/* const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL, // full production URL,
-   timeout: 30000,
+
+}); */
+
+
+const API = axios.create({
+  baseURL: "https://mern-ai-chat.onrender.com", // localhost nahi
 });
-
-
-
 
 
 // ---------------------------
@@ -30,7 +30,7 @@ export const fetchHistory = (sessionId) =>
 // ---------------------------
 export async function* sendMessageStream(sessionId, message) {
   const response = await fetch(
-    `${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/api/chat/chat-stream`,
+    `${`https://mern-ai-chat.onrender.com`}/api/chat/chat-stream`,
     {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
